6ba726de07d434c721746cc32eb71d2f4681e8db
Katlego-Sekoele <sekoelekatlego@gmail.com>
2023-08-13 00:17:23 +0200

    Register new quill module and crediting original author

--- src/lib/util/editor/quill.illustration.ts
+++ src/lib/util/editor/quill.illustration.ts
@@ -1,3 +1,21 @@
+// An altered copy of quill-comment by nhaouari - https://github.com/nhaouari/quill-comment
+/* In accordance wit nhaouari orignal MIT Licence:
+
+Copyright (c) 2017 by Win Min Tun
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 import Quill, {RangeStatic} from "quill";
 import Delta from "quill-delta";
 import Attributor from "parchment/src/attributor/attributor";
@@ -187,3 +205,15 @@ class QuillIllustration {
     }*/
 
 }
+
+export const DEFAULTS = {
+    illustrationAuthorId: null,
+    color: 'transparent',
+    enabled: false,
+    illustrationAddClick: null,
+    illustrationsClick: null,
+    illustrationTimestamp: null,
+    illustrationAddOn: null, // additional info
+};
+
+Quill.register('modules/quillIllustration', QuillIllustration);
