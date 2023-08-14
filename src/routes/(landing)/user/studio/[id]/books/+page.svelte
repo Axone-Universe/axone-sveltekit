cf431b28032a633fe75d1e713a86ba105dfd3ca6
Ben Cleveland <bnjcle001@myuct.ac.za>
2023-08-14 19:03:39 +0200

    Displaying current books on books tab

--- src/routes/(landing)/user/studio/[id]/books/+page.svelte
+++ src/routes/(landing)/user/studio/[id]/books/+page.svelte
@@ -1,10 +1,37 @@
 
 
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { page } from '$app/stores';
+	import type { HydratedDocument } from 'mongoose';
+	let currentPlace = $page.params
+	
+	
+	
+	export let data: PageData;
+	$: ({ session, UserBooks } = data);
 
+	function printer(){
+		console.log(UserBooks.length);
+	}
+	
+
+	</script>
 
 <div class="container p-10 space-y-4">
 	<h1>BOOKS</h1>
+	<ul>
+		//users is array and user is an alias
+		{#each UserBooks as book}
+		   <li>{book.title}</li>
+		   <li>{book.description}</li>
+		   <hr />
+		{/each}
+	
+	</ul>
 	<hr />
 	
-	
+	<button on:click={printer}>
+		Clicked  
+	</button>
 </div>
