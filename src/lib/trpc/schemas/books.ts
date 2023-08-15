f7925ca7949eba7f125c2839c17d4cda5d18da72
takundachirema <takunda.chirema@alumni.uct.ac.za>
2023-08-15 19:18:18 +0200

    feature: book UI for permissions

--- src/lib/trpc/schemas/books.ts
+++ src/lib/trpc/schemas/books.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod';
 import { genres } from './shared';
+import { permissions } from './permissions';
 
 export const create = z.object({
 	title: z.string(),
 	description: z.string(),
 	imageURL: z.string(),
-	genres: genres.optional()
+	genres: genres.optional(),
+	permissions: z.array(permissions).optional()
 });
 
 export const submitToCampaign = z.object({
