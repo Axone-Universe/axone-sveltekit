bdf83cc4df9e00c0b45a67e9e2286eabbc032075
Ben Cleveland <bnjcle001@myuct.ac.za>
2023-08-16 17:22:43 +0200

    Table of books working for book data

--- src/routes/(landing)/user/studio/[id]/books/+page.svelte
+++ src/routes/(landing)/user/studio/[id]/books/+page.svelte
@@ -8,6 +8,8 @@
 	import { page } from '$app/stores';
 	import type { HydratedDocument } from 'mongoose';
 	import { onMount } from 'svelte';
+	import { beforeUpdate } from 'svelte';
+	
 
 
 
@@ -19,93 +21,87 @@
 	let numBooks = 0;
 
 	export let data: PageData;
-	$: ({ session, UserBooks } = data);
+	$: ({ session, UserBooks} = data);
 
 
-	function BookCounter(){
-		numBooks = UserBooks.length;
-	}
+	//function BookCounter(){
+	//	numBooks = UserBooks.length;
+	//}
 	
 
 	type bookItem = {
 	position: number;
   	Book: string;
-  	description: string;
-  	date: Date;
-  	storylines: number;
+  	Description: string;
+  	Date: Date;
+  	Storylines: number;
 	};
-	const sourceData: Array<bookItem> = [];
 
-	for (let i = 0; i < numBooks; i++) {
 
-	const bookToAdd = ({
-	position: i,
-	Book: UserBooks[i].title,
-	description: UserBooks[i].description,
-	date: UserBooks[i].date,
-	storylines: UserBooks[i].storylines
-	});
-	//const bookToAdd = {position: i, Book: UserBooks[i].title, Description: UserBooks[i].description, Date: UserBooks[i].date, Storylines: UserBooks[i].storylines }
-	//console.log(UserBooks[i].title)
-	//console.log(bookToAdd)
-	sourceData.push(bookToAdd);
-	}
+	let sourceData: Array<bookItem> = [];
+	//const reactiveSourceData = reactive(sourceData);
+	function setup(){
+	for (let i = 0; i < UserBooks.length; i++) {
+
+		const bookToAdd = ({
+		position: i,
+		Book: UserBooks[i].title,
+		Description: UserBooks[i].description,
+		Date: UserBooks[i].date,
+		Storylines: UserBooks[i].storylines
+		});
 	
-
+		sourceData.push(bookToAdd);
+	}
+	console.log(sourceData.length);
+	sourceData = sourceData;
+    }
+	
+	//beforeUpdate(() => {
+    //console.log('the component is about to update');
+    //});
 	onMount(() => {
-	BookCounter();
-
-
-
+		setup();
 	});
+
+
+
+	//});
 	//BookCounter();
 	function printer(){
-		console.log(numBooks);
+		console.log(UserBooks.length);
 	}
 	//let numBooks = UserBooks.length;
 	
 	
 	function PRONTER(){
+	console.log(sourceData.length);
+	
 	for (let i = 0; i < sourceData.length; i++) {
-	//const bookToAdd = {position: i, Book: UserBooks[i].title, Description: UserBooks[i].description, Date: UserBooks[i].date, Storylines: UserBooks[i].storylines }
-	//console.log(UserBooks[i].title)
 	console.log(sourceData[i])
 	
 	}
 	}
+	console.log("TESTING" + sourceData.length)
+	console.log("THis is being called")
 
-	//const tableSimple: TableSource = {
-	// A list of heading labels.
-	//head: ['Book', 'Description', 'Date', 'Storylines'],
-	// The data visibly shown in your table body UI.
-	//body: tableMapperValues(sourceData, ['Book', 'Description', 'Date', 'Storylines']),
-	// Optional: The data returned when interactive is enabled and a row is clicked.
-	//meta: tableMapperValues(sourceData, ['position', 'Book', 'Description', 'Date', 'Storylines']),
+
 	
-//};
+	$: tableSimple = {
+	// A list of heading labels.
+	head: ['Book', 'Description', 'Date', 'Storylines'],
+	// The data visibly shown in your table body UI.
+	body: tableMapperValues(sourceData, ['Book', 'Description', 'Date', 'Storylines']),
+	// Optional: The data returned when interactive is enabled and a row is clicked.
+	meta: tableMapperValues(sourceData, ['position', 'Book', 'Description', 'Date', 'Storylines']),
+	
+	};
 	
 
 	</script>
 
 <div class="container p-10 space-y-4">
 	<h1>BOOKS</h1>
-	<ul>
-		//users is array and user is an alias
-		{#each UserBooks as book}
-		   <li>{book.title}</li>
-		   <li>{book.description}</li>
-		   <hr />
-		{/each}
-		
 	
-	</ul>
-	<hr />
-	
-	<button on:click={printer}>
-		Print  
-	</button>
-
-	<button on:click={PRONTER}>
-		PRONT  
-	</button>
+	<Table source={tableSimple} />
 </div>
