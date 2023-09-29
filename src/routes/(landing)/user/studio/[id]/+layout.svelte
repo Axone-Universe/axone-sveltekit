b6fcb99473df496ea67e96069f49593e6e9ca8af
Ben Cleveland <bnjcle001@myuct.ac.za>
2023-09-29 19:07:20 +0200

    changed to listboxes

--- src/routes/(landing)/user/studio/[id]/+layout.svelte
+++ src/routes/(landing)/user/studio/[id]/+layout.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import {goto} from '$app/navigation';
 
 	// Skeleton Features
+
+
+	/*<nav class="list-nav p-4">
+			<div>
+			
+				<a href={`../${currentPlace.id}/books`} on:click={() => { activeTab = 'books' }} class="{activeTab === 'books' ? 'bg-indigo-950' : ''}" >Books</a>
+				<a href={`../${currentPlace.id}/chapters`}  on:click={() => { activeTab = 'chapters' }} class="{activeTab === 'chapters' ? 'bg-indigo-950' : ''}" >Chapters</a>
+				<a href={`../${currentPlace.id}/storylines`} on:click={() => { activeTab = 'storylines' }} class="{activeTab === 'storylines' ? 'bg-indigo-950' : ''}">Storylines</a>
+			</div>
+			
+		</nav>
+		
+		*/
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 	import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
 	import type { ModalSettings, ModalComponent, DrawerSettings } from '@skeletonlabs/skeleton';
 	// Local Features
-	import Navigation from '$lib/components/navigation/Navigation.svelte';
+
 
 	// Stylesheets
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
@@ -23,7 +37,7 @@
 	
 
 	let currentPlace = $page.params;
-	$:activeTab = ''; 
+	$:activeTab = 'books'; 
 
 	//import '../app.postcss';
 	const drawerSettings: DrawerSettings = {
@@ -40,19 +54,28 @@
 
 	// Reactive Properties
 	$: classesSidebarLeft = $page.url.pathname === '/' ? 'w-0' : 'w-0 lg:w-64';
+
+
+	function goToBooks(){
+		activeTab = 'books';
+		goto(`../${currentPlace.id}/books`);
+	}
+	function goToChapters(){
+		activeTab = 'chapters';
+		goto(`../${currentPlace.id}/chapters`);
+	}
+	function goToStorylines(){
+		activeTab = 'storylines';
+		goto(`../${currentPlace.id}/storylines`);
+	}
 </script>
 
 
 
-<Drawer>
-	<h2 class="p-4">Navigation</h2>
-	<hr />
-	<Navigation />
-</Drawer>
 
 
 <!-- App Shell -->
-<AppShell slotSidebarLeft=" {classesSidebarLeft}">
+<AppShell slotSidebarLeft=" {classesSidebarLeft}" class="flex flex-col h-full">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
 		<AppBar>
@@ -67,22 +90,27 @@
 	</svelte:fragment>
 	<!-- Left Sidebar Slot -->
 	<svelte:fragment slot="sidebarLeft">
-		<nav class="list-nav p-4">
-			<div>
-				<ListBox>
-					<ListBoxItem>
-				<a href={`../${currentPlace.id}/books`} on:click={() => { activeTab = 'books' }} class="{activeTab === 'books' ? 'bg-indigo-950' : ''}" >Books</a>
-			</ListBoxItem>
-			<ListBoxItem>
-				<a href={`../${currentPlace.id}/chapters`}  on:click={() => { activeTab = 'chapters' }} class="{activeTab === 'chapters' ? 'bg-indigo-950' : ''}" >Chapters</a>
-			</ListBoxItem>
-			<ListBoxItem>
-				
-			<a href={`../${currentPlace.id}/storylines`} on:click={() => { activeTab = 'storylines' }} class="{activeTab === 'storylines' ? 'bg-indigo-950' : ''}">Storylines</a>
-		</ListBoxItem>	
+		<nav class="list-nav p-4 flex flex-col h-full" >
+			<div class="border-r border-gray-300 pr-4 h-full flex flex-col">
+				<ListBox class="flex flex-col h-full" >
+					<ListBoxItem name="{activeTab === 'books' ? '' : 'medium'}" on:click={() => { console.log(activeTab) ; goToBooks()}} class="{activeTab === 'books' ? 'bg-indigo-950' : 'soft-listbox'}" value="{activeTab === 'books' ? '' : 'book'}">
+						Books
+					</ListBoxItem >
+			
+					<ListBoxItem name="{activeTab === 'chapters' ? '' : 'medium'}" on:click={() => { console.log(activeTab) ; goToChapters()}} class="{activeTab === 'chapters' ? 'bg-indigo-950' : 'soft-listbox'}" value="{activeTab === 'chapters' ? '' : 'book'}">
+						Chapters
+					</ListBoxItem >
+					
+					<ListBoxItem name="{activeTab === 'storylines' ? '' : 'medium'}" on:click={() => {  console.log(activeTab) ; goToStorylines()}} class="{activeTab === 'storylines' ? 'bg-indigo-950' : 'soft-listbox'}" value="{activeTab === 'storylines' ? '' : 'book'}">
+						Storylines
+					</ListBoxItem>	
+				</ListBox>
 		</div>
-		</ListBox>
-		</nav>
+	</nav>
+		
+
+	
+		
 
 
 
@@ -96,15 +124,6 @@
 
 
 
-		<nav class="list-nav p-4">
-			<div>
-			
-				<a href={`../${currentPlace.id}/books`} on:click={() => { activeTab = 'books' }} class="{activeTab === 'books' ? 'bg-indigo-950' : ''}" >Books</a>
-				<a href={`../${currentPlace.id}/chapters`}  on:click={() => { activeTab = 'chapters' }} class="{activeTab === 'chapters' ? 'bg-indigo-950' : ''}" >Chapters</a>
-				<a href={`../${currentPlace.id}/storylines`} on:click={() => { activeTab = 'storylines' }} class="{activeTab === 'storylines' ? 'bg-indigo-950' : ''}">Storylines</a>
-			</div>
-			
-		</nav>
 		
 
 
