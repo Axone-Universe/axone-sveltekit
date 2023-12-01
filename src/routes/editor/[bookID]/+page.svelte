b756fedc3faf4f5cd5bbb51788d79b57ae1634d3
Katlego-Sekoele <sekoelekatlego@gmail.com>
2023-12-01 21:48:34 +0200

    Generated text appears in the editor

--- src/routes/editor/[bookID]/+page.svelte
+++ src/routes/editor/[bookID]/+page.svelte
@@ -764,22 +764,6 @@
 	}
 
 	/**
-	 * Ai generate text using current selection
-	 */
-	function aiAddClick() {
-		if (quill.oldSelectedRange === quill.selectedRange) {
-			return; // same range is selected
-		}
-
-		quill.getModule('ai').addAi({
-			//response from backend
-		});
-		quill.oldSelectedRange == quill.selectedRange;
-
-		showAi = true;
-	}
-
-	/**
 	 * Shows the illustration modal
 	 * @param illustration
 	 */
