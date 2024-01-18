14957e0cc9fdb2542cb9c64c749d656da0a1be81
takundachirema <takunda.chirema@alumni.uct.ac.za>
2024-01-18 13:44:12 +0200

    feature: tutorial for book creation

---
+++ src/routes/(landing)/book/create/tutorial.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import type Shepherd from 'shepherd.js';
+	import 'shepherd.js/dist/css/shepherd.css';
+	import { onMount } from 'svelte';
+	import { Icon } from 'svelte-awesome';
+	import { info } from 'svelte-awesome/icons';
+	import { back, complete, next, tour, getShepherdStep } from '$lib/util/tour/tour';
+
+	onMount(() => {
+		setupTour();
+	});
+
+	export function setupTour() {
+		tour.addStep(getShepherdStep('book-title', 'bottom', 'Insert book title', [back, next]));
+
+		tour.addStep(
+			getShepherdStep('book-description', 'bottom', 'Insert book description', [back, next])
+		);
+
+		tour.addStep(
+			getShepherdStep('image-uploader-div', 'bottom', 'Upload the book cover', [back, next])
+		);
+
+		tour.addStep(
+			getShepherdStep('genres-div', 'bottom', 'Select book genres for readers to find your book', [
+				back,
+				next
+			])
+		);
+
+		tour.addStep(
+			getShepherdStep(
+				'permission-users-input',
+				'bottom',
+				'Search for users to give permissions to your book',
+				[back, next]
+			)
+		);
+
+		tour.addStep(
+			getShepherdStep(
+				'public-permissions-btn',
+				'bottom',
+				'Set permissions for the public. <br>&#8226; View allows public viewing for your book. <br>&#8226; Collaborate allows the public to create new storylines for your book.',
+				[back, complete]
+			)
+		);
+	}
+</script>
+
+<button class="fixed z-50 bottom-10 right-10 btn-icon variant-filled" on:click={() => tour.start()}>
+	<Icon data={info} scale={1.5} />
+</button>
