b9dbc2c1e8b238ee19b3a658ffa0e6cc4dcd0972
Axone-Universe <135121326+Axone-Universe@users.noreply.github.com>
2025-01-17 07:51:54 +0200

    Small fixes (#132)
    * fix: single node docker replica set
    
    * fix: users infite loading
    
    * fix: UI fixes
    
    * fix: put a requirement for a book cover when creating a book
    
    * fix: put a requirement for a book cover when creating a storyline
    
    * fix: Update the correct chapter delta after autosave
    
    Pass all chapters to quill.js and update the correct chapter when there is a network lag
    
    * fix: Tests fix
    
    * fix: mobile drawer, editor/reader toggle, write book cancel button
    
    * fix: made storyline title to be the main title & edited main campaign storyline permissions
    
    * feature: added updated field to the delta model
    
    * fixes: various fixes
    
    added poetry genre
    
    added lock for locked books and storylines
    
    fixed image uploads when saving books and storylines
    
    added campaigns on its own button in the studio
    
    * fix: show book image if storyline has no image
    
    * feature: change infinite scrolling to use intersection observer
    
    * fix: delete campaigns together with the books
    
    * feature: add social media sharing on books
    
    * fix: padding on book view
    
    * feature: added error page in routes folder
    
    ---------
    
    Co-authored-by: takundachirema <takunda.chirema@alumni.uct.ac.za>

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/stores';
+	import Container from '$lib/components/Container.svelte';
+</script>
+
+<Container class="h-screen flex flex-col justify-center items-center">
+	<div class="text-6xl">{$page.status}</div>
+	<div>{$page.error?.message}</div>
+</Container>
